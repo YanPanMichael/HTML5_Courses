ad05f4e334e40df2fdd0cac566862f1e04e6e569
YanPanMichael <yanpancsu@icloud.com>
2018-12-10 00:26:45 +0800

    selection sorting by python

--- Sorting/SelectionSorting.js
+++ Sorting/SelectionSorting.js
@@ -16,3 +16,11 @@ function selectionSort(arr) {
 }
 
 console.log(selectionSort([2,4,3,3,6,3,10,1,7,5]));
+
+//python
+// def selectionSort(arr):
+//     for i in range(len(arr)-1):
+//         for j in range(i+1, len(arr)):
+//             if arr[j] < arr[i]:
+//                 arr[i], arr[j] = arr[j], arr[i]
+//     return arr
