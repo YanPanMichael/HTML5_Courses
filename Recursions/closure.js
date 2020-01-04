cffbcf3c9566303e73ee57c53baad379ee620400
YanPanMichael <yanpancsu@icloud.com>
2020-01-05 00:04:18 +0800

    save

--- Recursions/closure.js
+++ Recursions/closure.js
@@ -1,26 +1,26 @@
-function createFunctions() {
-  var result = new Array();
-  for (let i = 0; i < 10; i++) {
-    result[i] = function() {
-      return i;
-    }();
-  }
-  return result;
-}
+// function createFunctions() {
+//   var result = new Array();
+//   for (let i = 0; i < 10; i++) {
+//     result[i] = function() {
+//       return i;
+//     }();
+//   }
+//   return result;
+// }
 
-console.log(createFunctions());
+// console.log(createFunctions());
 
-function createFunctions2() {
-  var result = new Array();
-  for (let i = 0; i < 10; i++) {
-    result[i] = (function(num) {
-      return num;
-    })(i);
-  }
-  return result;
-}
+// function createFunctions2() {
+//   var result = new Array();
+//   for (let i = 0; i < 10; i++) {
+//     result[i] = (function(num) {
+//       return num;
+//     })(i);
+//   }
+//   return result;
+// }
 
-console.log(createFunctions2());
+// console.log(createFunctions2());
 
 function F() {
   var a = 100
@@ -28,3 +28,8 @@ function F() {
     console.log(a)
   }
 }
+
+var f = F();
+var a = 200;
+f(); // 100
+//根据定义 而非 运行时的作用域链 a在定义时父级成员a的值是100
