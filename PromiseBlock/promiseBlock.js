f9ea692b5adb44d35b48a782fcc0c7cbc7530ef7
YanPanMichael <yanpancsu@icloud.com>
2020-04-24 00:45:14 +0800

    promist and await

--- PromiseBlock/promiseBlock.js
+++ PromiseBlock/promiseBlock.js
@@ -1,6 +1,25 @@
 function promBlock(props) {
-  const {resolve, reject} = props;
-  new Promise(() => {
-    resolve()
+  new Promise((resolve, reject) => {
+    const props = {
+      resolve, 
+      reject,
+    };
+    fetchData(props);
+  }).then((res) => {
+    if(res.status === 'succeed') {
+      console.log("succeed");
+    }
+  }, (err) => {
+    console.log("fail", err);
   })
+}
+
+async function fetchData(props) {
+  const { resolve, reject } = props;
+  try {
+    const result = await fetch();
+    resolve(result.data);
+  } catch (error) {
+    reject(error);
+  }
 }
